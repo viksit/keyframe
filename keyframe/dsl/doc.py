d04f23575f254eb4bfb56b1bd80df9eb258f090a
viksit <viksit@gmail.com>
2016-10-31 15:22:07 +0530

    Using keyframe to: - deploy to lambda via zappa deploy dev - channel http and facebook reused and working - keyframe v0.2 to do slot and intent filling as well as simpler boilerplate   free code - add first version of the DSL  - this is not complete, WIP - add design and notes

---
+++ keyframe/dsl/doc.py
@@ -0,0 +1,256 @@
+from __future__ import print_function
+import sys
+import os
+import re
+
+from six import iteritems, add_metaclass
+
+from field import Field
+from mapping import Mapping
+from utils import ObjectBase, AttrDict, merge
+from exc import ValidationException
+
+DELETE_META_FIELDS = frozenset((
+    "id", "parent", "routing", "version", "version_type"
+))
+
+DOC_META_FIELDS = frozenset((
+    "timestamp", "ttl"
+)).union(DELETE_META_FIELDS)
+
+META_FIELDS = frozenset((
+    # Elasticsearch metadata fields, except "type"
+    "index", "using", "score",
+)).union(DOC_META_FIELDS)
+
+class ResultMeta(AttrDict):
+    def __init__(self, document, exclude=("Meta")):
+        d = dict((k[1:] if k.startswith("_") else k, v) for (k, v) in iteritems(document) if k not in exclude)
+        if "type" in d:
+            # make sure we are consistent everywhere in python
+            d["doc_type"] = d.pop("type")
+        super(ResultMeta, self).__init__(d)
+
+class MetaField(object):
+    def __init__(self, *args, **kwargs):
+        self.args, self.kwargs = args, kwargs
+
+class DocTypeMeta(type):
+    def __new__(cls, name, bases, attrs):
+        # DocTypeMeta filters attrs in place
+        attrs["_doc_type"] = DocTypeOptions(name, bases, attrs)
+        return super(DocTypeMeta, cls).__new__(cls, name, bases, attrs)
+
+class DocTypeOptions(object):
+    def __init__(self, name, bases, attrs):
+
+        # set udf
+        self.udf = dict([i for i in attrs.items() if not i[0].startswith("__")])
+
+        meta = attrs.pop("Meta", None)
+
+        # default index, if not overriden by doc.meta
+        self.index = getattr(meta, "index", None)
+        self.description = getattr(meta, "description", None)
+        self.train_file = getattr(meta, "train_file", None)
+        self.test_file = getattr(meta, "test_file", None)
+        self.model_name = getattr(meta, "model_name", None)
+
+        # default cluster alias, can be overriden in doc.meta
+        self._using = getattr(meta, "using", None)
+
+        # get doc_type name, if not defined take the name of the class and
+        # transform it to lower_case
+        doc_type = getattr(meta, "doc_type",
+                           re.sub(r"(.)([A-Z])", r"\1_\2", name).lower())
+
+        # create the mapping instance
+        self.mapping = getattr(meta, "mapping", Mapping(doc_type))
+
+        # register all declared fields into the mapping
+        for name, value in list(iteritems(attrs)):
+            if isinstance(value, Field):
+                self.mapping.field(name, value)
+                del attrs[name]
+
+        # add all the mappings for meta fields
+        for name in dir(meta):
+            if isinstance(getattr(meta, name, None), MetaField):
+                params = getattr(meta, name)
+                self.mapping.meta(name, *params.args, **params.kwargs)
+
+        # document inheritance - include the fields from parents" mappings and
+        # index/using values
+        for b in bases:
+            if hasattr(b, "_doc_type") and hasattr(b._doc_type, "mapping"):
+                self.mapping.update(b._doc_type.mapping, update_only=True)
+                self._using = self._using or b._doc_type._using
+                self.index = self.index or b._doc_type.index
+                self.model_name = self.model_name or b._doc_type.model_name
+
+
+    @property
+    def using(self):
+        return self._using or "default"
+
+    @property
+    def name(self):
+        return self.mapping.properties.name
+
+    @property
+    def parent(self):
+        if "_parent" in self.mapping._meta:
+            return self.mapping._meta["_parent"]["type"]
+        return
+
+    def init(self, index=None, using=None):
+        self.mapping.save(index or self.index, using=using or self.using)
+
+    def refresh(self, index=None, using=None):
+        self.mapping.update_from_es(index or self.index, using=using or self.using)
+
+
+@add_metaclass(DocTypeMeta)
+class IntentModel(ObjectBase):
+
+    def __init__(self, meta=None, base=None, **kwargs):
+        meta = meta or {}
+        base = base or {}
+        for k in list(kwargs):
+            if k.startswith("_") and k[1:] in META_FIELDS:
+                meta[k] = kwargs.pop(k)
+
+        udf = self._doc_type.udf
+
+        if self._doc_type.model_name:
+            meta.setdefault("_model_name", self._doc_type.model_name)
+        if self._doc_type.description:
+            meta.setdefault("_description", self._doc_type.description)
+        if self._doc_type.train_file:
+            meta.setdefault("_train_file", self._doc_type.train_file)
+        if self._doc_type.test_file:
+            meta.setdefault("_test_file", self._doc_type.test_file)
+        if self._doc_type.index:
+            meta.setdefault("_index", self._doc_type.index)
+
+        super(AttrDict, self).__setattr__("meta", ResultMeta(meta))
+        super(AttrDict, self).__setattr__("udf", ResultMeta(udf))
+        super(IntentModel, self).__init__(**kwargs)
+
+    def __getstate__(self):
+        return (self.to_dict(), self.meta._d_)
+
+    def __setstate__(self, state):
+        data, meta = state
+        super(AttrDict, self).__setattr__("_d_", data)
+        super(AttrDict, self).__setattr__("meta", ResultMeta(meta))
+
+
+    def __getattr__(self, name):
+        if name.startswith("_") and name[1:] in META_FIELDS:
+            return getattr(self.meta, name[1:])
+        return super(IntentModel, self).__getattr__(name)
+
+
+
+    def __repr__(self):
+        return "%s(%s)" % (
+            self.__class__.__name__,
+            ", ".join("%s=%r" % (key, getattr(self.meta, key)) for key in
+                      ("index", "doc_type", "id") if key in self.meta)
+        )
+
+    def __setattr__(self, name, value):
+        if name.startswith("_") and name[1:] in META_FIELDS:
+            return setattr(self.meta, name[1:], value)
+        return super(IntentModel, self).__setattr__(name, value)
+
+    @classmethod
+    def init(cls, index=None, using=None):
+        cls._doc_type.init(index, using)
+
+    @classmethod
+    def search(cls, using=None, index=None):
+        pass
+
+    @classmethod
+    def get(cls, id, using=None, index=None, **kwargs):
+        pass
+
+    @classmethod
+    def mget(cls, docs, using=None, index=None, raise_on_error=True,
+             missing="none", **kwargs):
+        pass
+
+    @classmethod
+    def from_es(cls, hit):
+        # don"t modify in place
+        meta = hit.copy()
+        doc = meta.pop("_source", {})
+
+        if "fields" in meta:
+            for k, v in iteritems(meta.pop("fields")):
+                if k == "_source":
+                    doc.update(v)
+                if k.startswith("_") and k[1:] in META_FIELDS:
+                    meta[k] = v
+                else:
+                    doc[k] = v
+
+        return cls(meta=meta, **doc)
+
+    def _get_index(self, index=None):
+        if index is None:
+            index = getattr(self.meta, "index", self._doc_type.index)
+        if index is None:
+            raise ValidationException("No index")
+        return index
+
+    def delete(self, using=None, index=None, **kwargs):
+        es = self._get_connection(using)
+        # extract parent, routing etc from meta
+        doc_meta = dict(
+            (k, self.meta[k])
+            for k in DELETE_META_FIELDS
+            if k in self.meta
+        )
+        doc_meta.update(kwargs)
+        es.delete(
+            index=self._get_index(index),
+            doc_type=self._doc_type.name,
+            **doc_meta
+        )
+
+    def to_dict(self, include_meta=False):
+        d = super(IntentModel, self).to_dict()
+        if not include_meta:
+            return d
+
+        meta = dict(
+            ("_" + k, self.meta[k])
+            for k in DOC_META_FIELDS
+            if k in self.meta
+        )
+
+        # in case of to_dict include the index unlike save/update/delete
+        if "index" in self.meta:
+            meta["_index"] = self.meta.index
+        elif self._doc_type.index:
+            meta["_index"] = self._doc_type.index
+
+        meta["_type"] = self._doc_type.name
+        meta["_source"] = d
+        return meta
+
+
+    def save(self, **kwargs):
+        print("save to server")
+
+    def train(self, **kwargs):
+        print("train model")
+
+class EntityModel(object):
+    pass
+
+class AgentModel(object):
+    pass
--- keyframe/main.py
+++ keyframe/main.py
@@ -5,14 +5,31 @@ import logging
 import messages
 import channel_client
 import fb
+import config
+
+import uuid
+from collections import defaultdict
+import sys
 
 log = logging.getLogger(__name__)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+logformat = "[%(levelname)1.1s %(asctime)s %(name)s] %(message)s"
+formatter = logging.Formatter(logformat)
+ch.setFormatter(formatter)
+log.addHandler(ch)
+log.setLevel(logging.DEBUG)
+log.propagate = False
 
+
+def getUUID():
+   return str(uuid.uuid4()).replace("-", "")
 
 
 ################# Library code #####################
 
 class CmdLineHandler(object):
+
     def __init__(self, userId=None):
         self.userId = userId
         if not self.userId:
@@ -24,6 +41,7 @@ class CmdLineHandler(object):
         pass
 
     def begin(self):
+
         while True:
             try:
                 userInput = raw_input("> ")
@@ -66,6 +84,476 @@ class BotCmdLineHandler(CmdLineHandler):
         self.bot.process(canonicalMsg)
 
 
+class BotAPI(object):
+    """
+    Class that allows this bot to be called via a flask API. This can be deployed
+    wherever
+    """
+    def __init__(self, channelClient):
+        self.channelClient = channelClient
+
+    def createAndSendTextResponse(self, canonicalMsg, text, responseType=None):
+        cr = messages.createTextResponse(canonicalMsg, text, responseType)
+        self.channelClient.sendResponse(cr)
+
+    def handleMsg(self, channelMsg):
+        """Handle the input message from all channels.
+        Input
+          inputMsg: (messages.ChannelMsg)
+        Returns
+          nothing
+        """
+        canonicalMsg = self.channelClient.extract(
+            channelMsg=channelMsg)
+        if not canonicalMsg:
+            log.warn("no canonicalMsg extracted from channelMsg (%s)", channelMsg)
+            return
+        # The bot to be created may depend on the user.
+        bot = self.getBot()
+        bot.setChannelClient(self.channelClient)
+        bot.process(canonicalMsg)
+
+    def getBot(self):
+        return self.bot
+
+    @classmethod
+    def getChannelClient(cls):
+        channelClient = channel_client.ChannelClient()
+        return channelClient
+
+    @classmethod
+    def requestHandler(cls, event, context):
+        log.info("event: %s, context: %s",
+                 event, context)
+
+        # Check if this is a GET request from FB. If yes, it is a verify.
+        # Handle it.
+        if event.get("channel") == messages.CHANNEL_FB \
+           and event.get("request-type") == "GET":
+            return fb.gateway_webhook_verify_handler(
+                event, context)
+
+        channelMsg = messages.ChannelMsg(
+            channel=event.get("channel"),
+            httpType=event.get("request-type"),
+            body=event.get("body"))
+
+        channelClient = channel_client.getChannelClient(
+            channel=event.get("channel"),
+            requestType=event.get("request-type"),
+            config=config.Config())
+
+        botAPI = cls(
+            channelClient=channelClient
+        )
+        print("botapi is: ", botAPI)
+        botAPI.handleMsg(channelMsg)
+        print(">> channel client in botapi is:", channelClient)
+        print(">> botapi: chanel client is ", channelClient.responses)
+        resp = channelClient.popResponses()
+        log.info("BotAPI.requestHandler returning: %s", resp)
+        return str(resp)
+
+
+# Class based decorators
+class ActionObject(object):
+
+    def __init__(self):
+        self.__clsid__ = getUUID()
+        # TODO(viksit): dont make this manually assignable?
+        # Dynamically injected
+        self.apiResult = None
+        self.canonicalMsg = None
+
+    def process(self):
+        pass
+
+class BotState(object):
+
+    """Serializable object for keeping bot state across requests.
+    """
+
+    def __init__(self):
+        self._waiting = None
+        # Json-compatible structure that allows
+        self._lastResult = None  # CanonicalResult
+        self.changed = False
+        self.debug = False
+
+    def __repr__(self):
+        return "%s" % (self.toJSONObject())
+
+    def setDebug(self, debug):
+        self.debug = debug
+
+    def getWaiting(self):
+        """Get an action object waiting for input, remove it
+        from waiting, and return it (like a pop except this is not a stack).
+        """
+        tmp = self._waiting
+        self._waiting = None
+        self.changed = True
+        return tmp
+
+    def putWaiting(self, actionObjectJson):
+        """Input
+        actionObjectJson: (object) A json-compatible python data structure
+        """
+        self._waiting = actionObjectJson
+        self.changed = True
+
+    def getLastResult(self):
+        return self._lastResult
+
+    def putLastResult(self, canonicalResult):
+        self._lastResult = canonicalResult
+        self.changed = True
+
+    def toJSONObject(self):
+        """Return a json-compatible data structure with all of the
+        data required to reconstruct this instance.
+        """
+        return {
+            "class":self.__class__.__name__,
+            "waiting":self._waiting,
+            "last_result": self._lastResult
+        }
+
+    @classmethod
+    def fromJSONObject(cls, jsonObject):
+        """Input
+        jsonObject: object as returned by toJSONObject()
+        """
+        assert jsonObject.get("class") == cls.__name__, \
+            "bad class. data class: %s, my class: %s" % (
+                jsonObject.get("class"), cls.__name__)
+        botState = cls()
+        botState._waiting = jsonObject.get("waiting")
+        botState._lastResult = jsonObject.get("last_result")
+        return botState
+
+
+class Slot(object):
+
+    def __init__(self):
+        pass
+
+    def init(self, **kwargs):
+        self.name = kwargs.get("name")
+        self.entityType = kwargs.get("entityType")
+        self.required = kwargs.get("required")
+        self.intent = kwargs.get("intent")
+        self.filled = False
+        self.value = None
+        self.validated = False
+
+    def get(self):
+        pass
+
+    def prompt(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def reset(self):
+        # Only change the modifiable stuff
+        self.value = None
+        self.validated = False
+        self.filled = False
+
+class BaseBotv2(object):
+
+    def __init__(self, *args, **kwargs):
+
+        self.api = kwargs.get("api")
+        self.channelClient = kwargs.get("channelClient")
+        self.ctxstore = kwargs.get("ctxstore")
+        self.config = kwargs.get("config")
+        self.debug = kwargs.get("debug")
+
+        self.intentActions = {}
+        self.intentThresholds = {}
+        self.keywordIntents = {}
+        self.regexIntents = {}
+        self.intentSlots = defaultdict(lambda: [])
+        # Add debug
+        self.init()
+
+        self.state = "new"
+        self.onetime = False
+
+    def init(self):
+        # Override to initialize stuff in derived bots
+        pass
+
+    def setChannelClient(self, cc):
+        self.channelClient = cc
+
+    def createAndSendTextResponse(self, canonicalMsg, text, responseType=None):
+        log.info("createAndSendTextResponse(%s)", locals())
+        cr = messages.createTextResponse(canonicalMsg, text, responseType)
+        log.info("cr: %s", cr)
+        self.channelClient.sendResponse(cr)
+
+    def errorResponse(self, canonicalMsg):
+        self.createAndSendTextResponse(
+            canonicalMsg, "Internal Error",
+            messages.ResponseElement.RESPONSE_TYPE_RESPONSE)
+
+    def process(self, canonicalMsg):
+        print("---------: process called")
+        return self.handle(
+            canonicalMsg=canonicalMsg,
+            myraAPI=self.api)
+
+    # Decorators
+    # keyword intent, regex intent
+    def intent(self, intentStr, **args):
+        def myfun(cls):
+            wrapped = cls(**args)
+            self.intentActions[intentStr] = wrapped
+
+            # TODO(viksit): is this needed anymore?
+            class Wrapper(object):
+                def __init__(self, *args):
+                    self.wrapped = cls(*args)
+                    self.intentActions[intentStr] = self.wrapped
+
+                def __getattr__(self, name):
+                    return getattr(self.wrapped, name)
+            # return class
+            return Wrapper
+
+        # return decorator
+        return myfun
+
+    # why shouldn't this be just regular keyword args?
+    def slot(self, intentStr, slotList, **args):
+        def myfun(cls):
+            wrapped = cls()
+            wrapped.init(intent=intentStr,
+                         name=slotList[0],
+                         required=slotList[1],
+                         entityType=slotList[2])
+            # TODO(viksit): error handling
+            self.intentSlots[intentStr].append(wrapped)
+        # return decorator
+        return myfun
+
+    def fillFrom(self, canonicalMsg, slotClasses, apiResult):
+        for slotClass in slotClasses:
+            slotClass.canonicalMsg = canonicalMsg
+            slotClass.apiResult = apiResult
+            if not slotClass.filled:
+                #print("trying to fill slot %s from within sentence" % slotClass.name)
+                e = apiResult.entities.entity_dict.get("builtin", {})
+                if slotClass.entityType in e:
+                    # TODO(viksit): this needs to change to have "text" in all entities.
+                    k = "text"
+                    if slotClass.entityType == "DATE":
+                        k = "date"
+                    tmp = [i.get(k) for i in e.get(slotClass.entityType)]
+
+                    if len(tmp) > 0:
+                        slotClass.value = tmp[0]
+                        slotClass.filled = True
+                        #print("\tslot was filled in this sentence")
+                        continue
+                    else:
+                        #print("\tslot wasn't filled in this sentence")
+                        # nothing was found
+                        # we'll query the user for it.
+                        pass
+                else:
+                    print("\tslot wasn't filled in this sentence")
+
+    """
+    Evaluate the given sentence to see which slots you can fill from it.
+    Mark the ones that are filled
+    The ones that remain unfilled are the ones that we come back to each time.
+    """
+    def fill(self, slotClasses, canonicalMsg, apiResult):
+
+
+        #print("Availble slots: ")
+        #for slotClass in slotClasses:
+        #    print("\t slot, filled", slotClass.name, slotClass.filled)
+
+        # First we should fill all possible slots from the sentence
+        # For those that aren't filled, we run through this logic.
+
+        #print("self onetime: ", self.onetime)
+        if not self.onetime:
+            self.fillFrom(canonicalMsg, slotClasses, apiResult)
+            self.onetime = True
+
+        # Now, whats left unfilled are slots that weren't completed by the user
+        # in the first go. Ask the user for input here.
+        print("++++++++++")
+        for slotClass in slotClasses:
+            # TODO(viksit): add validation step here as well.
+            if not slotClass.filled:
+                slotClass.canonicalMsg = canonicalMsg
+                slotClass.apiResult = apiResult
+                #print("trying to fill slot %s via user" % slotClass.name)
+                #print("state: ", self.state)
+                if self.state == "new":
+                    # We are going to ask user for an input
+                    responseType = messages.ResponseElement.RESPONSE_TYPE_RESPONSE
+                    cr = messages.createTextResponse(
+                        canonicalMsg,
+                        slotClass.prompt(),
+                        responseType)
+                    print(">>>>>> cr: ", cr)
+                    print(">>> channel client: ", self.channelClient)
+                    print(">>> self.cc: PRE", self.channelClient.responses)
+                    self.channelClient.sendResponse(cr)
+                    print(">> self.cc POST responses: ", self.channelClient.responses)
+                    self.state = "process_slot"
+                    botState["slotClasses"] = slotClasses
+                    return False
+
+                # Finalize the slot
+                elif self.state == "process_slot":
+                    # We will evaluate the user's input
+
+                    # TODO(viksit): this fillFrom function should refactor to slotClass.fill()
+                    # This function could then be overwritten by a keyframe user
+
+                    self.fillFrom(canonicalMsg, slotClasses, apiResult)
+                    slotClass.validate()
+                    self.state = "new"
+                    botState["slotClasses"] = slotClasses
+                    # continue to the next slot
+
+        ######################################
+        # End slot filling
+        # Now, all slots for this should be filled.
+        # check
+        allFilled = True
+        for slotClass in slotClasses:
+            if not slotClass.filled:
+                allFilled = False
+                break
+        self.state = "new"
+        #print("all filled is : ", allFilled)
+        return allFilled
+
+
+    def handle(self, **kwargs):
+        """ Support keyword intent, model intent and regex intent
+        handling
+        """
+        canonicalMsg = kwargs.get("canonicalMsg")
+        myraAPI = kwargs.get("myraAPI")
+
+        # TODO(viksit): add regex and keyword intents for the less advanced uses.
+        # If we got this far, then we need to run the model
+
+        # local testing
+        # ******************
+        apiResult = myraAPI.get(canonicalMsg.text)
+        intentStr = apiResult.intent.label
+        intent_score = apiResult.intent.score
+
+
+        """
+        when an utterance comes in, we send it to our API to get an intent response back.
+        once the intent is known, we determine whether it contains any slots.
+        we then give this to the slot processor (its a list of slot items)
+        the slot processor is the first thing that is run after we get the intent.
+        once the slots are filled we should make them available as a dictionary locally.
+
+        actionObject.slots = {
+          "person": {
+            "filled": false/true,
+            "value": "foo",
+            "entity_type": ENTITY_TYPE,
+            "required": true/false,
+            "validated": true/false
+          }, ..
+        }
+
+
+        before we handle the intent, slots need to be filled.
+
+        for slotclass in slotlist:
+          channel.send(slotclass.prompt())
+          slotValue = slotclass.get()
+          v = slotclass.validate(slotValue)
+          if v:
+            continue
+          else:
+            ask again (max tries of 3)
+
+        once we assert that all slots are filled, we run the process and give it slot information.
+
+        """
+
+        if self.state == "process_slot":
+            # avoid using the intent itself.
+            # continue the slot filling
+            #print("state is process_slot")
+            slotClasses = botState.get("slotClasses")
+
+            #print("we're in user fill mode")
+            allFilled = self.fill(slotClasses, canonicalMsg, apiResult)
+            # once the slots are filled, we need to get into the intent process
+            if allFilled is False:
+                #print("allFilled is false, so lets go back to the fill function")
+                #allFilled = self.fill(slotClasses, canonicalMsg, apiResult)
+                return
+
+            # All slots are now filled
+            intentStr = slotClasses[0].intent
+            # state is now back to "new"
+
+        if intentStr not in self.intentActions:
+            raise ValueError("Intent '{}'' has not been registered".format(intentStr))
+
+        # We have a valid intent.
+        # Does it have slots?
+        if intentStr not in self.intentSlots:
+            return
+
+        # TODO(viksit): serialize the intent-slot data structure
+        slotClasses = self.intentSlots.get(intentStr)
+        allFilled = self.fill(slotClasses, canonicalMsg, apiResult)
+        if not allFilled:
+            print("all slots were not filled")
+            return
+
+        # Get the actionObject
+        actionObject = self.intentActions.get(intentStr)
+
+        # Make slots available to actionObject
+        # Make the apiResult available within the scope of the intent handler.
+        import copy
+        # TODO(viksit): make slots a dict so it can be easily used by other people.
+        actionObject.slots = copy.deepcopy(slotClasses)
+        actionObject.apiResult = apiResult
+        actionObject.canonicalMsg = canonicalMsg
+        actionObject.messages = messages
+        actionObject.channelClient = self.channelClient
+        # Once the actionObject is returned, lets clean out any state we have
+        # Currently this doesn't actually return something.
+
+        print("state: %s" % self.state)
+
+        # Reset the slot state
+        for slotClass in slotClasses:
+            slotClass.reset()
+        self.onetime = False
+
+        return actionObject.process()
+
+
+botState = {}
+
+
+# Older
+######################################################
 class Actions(object):
     """ Intent-Action mapping function decorators
     """
@@ -172,3 +660,70 @@ class BaseBot(object):
 
 
 ############ End library code ###########################
+
+"""
+```
+
+
+keyframe initapp
+
+# creates a bot.py and a models.py as well as a config.py
+
+define the models.py
+
+
+from keyframe import intents, entities, agents
+
+class CalendarIntentModel(intents.Model):
+
+  mykeywords = ["foo", "bar", "baz"]
+  myregex = r"foo(.)+*"
+  create = intents.StatisticalIntent()
+  cancel = intents.StatisticalIntent()
+  foo = intents.RegexIntent(myregex)
+  bar = intents.KeywordIntent(mykeywords)
+
+  class Meta:
+    modelName = "calendar intent"
+    description = "foo"
+    trainFile = ".."
+    testFile = ".."
+
+
+class CalendarEntityModel(entities.Model):
+
+  mydict = ["foo", "bar", "baz"]
+
+  # allow files or simply read them here
+
+  myregex = r"foo(.)+*"
+
+  foo = intents.RegexEntity(myregex)
+  bar = intents.DictionaryEntity(mydict)
+
+  class Meta:
+    modelName = "calendar entities"
+    description = "foo"
+
+
+
+class CalendarAgent(agents.Agent):
+  prodIntent = ..
+  prodEntity = ..
+
+
+
+```
+
+then
+
+# set up myra key somewhere
+
+keyframe sync intents, entities
+keyframe train intents
+keyframe test
+
+
+
+
+"""
