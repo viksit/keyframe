7e78895b5807838229ff70e63eefe3631d2bd453
viksit <viksit@gmail.com>
2016-12-02 01:15:23 -0800

    add dslv1

---
+++ keyframe/dsl/doc.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import sys
+import os
+
+
+class Intent(object):
+    _name = None
+    _id = None
+
+    def __init__(self):
+        pass
+
+class Entity(object):
+    pass
+
+class Agent(object):
+    pass
