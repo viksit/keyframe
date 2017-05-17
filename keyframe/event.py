680f86f8e41d503505dd4d193a4a247668670192
Nishant Deshpande <nishant@myralabs.com>
2017-05-17 14:13:46 -0700

    sessions and event writing

---
+++ keyframe/event.py
@@ -0,0 +1,76 @@
+import random
+import time
+import json
+
+EVENT_VERSION = 2
+
+def createEvent(**kwargs):
+    _t = time.time()
+    tsMs = int(round(_t*1000))
+    ts = int(round(_t))
+    eventType = kwargs.get("eventType")
+    assert eventType, "must have eventType to create an event"
+    eventId = Event.createEventId(eventType, ts)
+    assert "ts" not in kwargs
+    assert "eventId" not in kwargs
+    kwargs["ts"] = ts
+    kwargs["tsMs"] = tsMs
+    kwargs["eventId"] = eventId
+    return Event(**kwargs)
+
+class Event(object):
+    @classmethod
+    def createEventId(cls, eventType, ts):
+        return "kf_%s_%i_%i" % (
+            eventType, ts, random.randint(0,1000))
+
+    def __init__(self, **kwargs):
+        self.version = kwargs.get("version")  # integer
+        if not self.version:
+            self.version = EVENT_VERSION
+        self.eventType = kwargs.get("eventType")  # request, response
+        self.src = kwargs.get("src")  # user, widget, agent
+        self.sessionStatus = kwargs.get("sessionStatus")  # start, end
+        self.eventId = kwargs.get("eventId")
+        self.sessionId = kwargs.get("sessionId")
+        self.userId = kwargs.get("userId")
+        self.ts = kwargs.get("ts")
+        self.tsMs = kwargs.get("tsMs")
+        self.topicId = kwargs.get("topicId")
+        self.topicType = kwargs.get("topicType")
+        self.slotId = kwargs.get("slotId")
+        self.slotType = kwargs.get("slotType")
+        self.actionType = kwargs.get("actionType")
+        self.responseType = kwargs.get("responseType")  # prompt, fill
+        self.payload = kwargs.get("payload")  # must be a json-compatible data structure (i.e. string, dict)
+        self.ticketFiled = kwargs.get("ticketFiled")
+        self.resolutionStatus = kwargs.get("resolutionStatus")
+
+    def toJSON(self):
+        return {
+            "resolution_status":self.resolutionStatus,
+            "ticket_filed":self.ticketFiled,
+            "version":self.version,
+            "event_type":self.eventType,
+            "src":self.src,
+            "session_status":self.sessionStatus,
+            "event_id":self.eventId,
+            "session_id":self.sessionId,
+            "user_id":self.userId,
+            "ts":self.ts,
+            "ts_ms":self.tsMs,
+            "topic_id":self.topicId,
+            "topic_type":self.topicType,
+            "slot_id":self.slotId,
+            "slot_type":self.slotType,
+            "action_type":self.actionType,
+            "response_type":self.responseType,
+            "payload":self.payload}
+
+    def toJSONStr(self):
+        return json.dumps(self.toJSON())
+
+    def __repr__(self):
+        return json.dumps(
+            self.toJSON(), sort_keys=True, indent=2,
+            separators=(',', ': '))
---
+++ keyframe/event_writer.py
@@ -0,0 +1,99 @@
+import os
+import datetime
+import logging
+import json
+
+import boto3
+
+import keyframe.config
+
+log = logging.getLogger(__name__)
+
+
+WRITER_TYPE_FILE = "file"
+WRITER_TYPE_KINESIS = "kinesis"
+WRITER_TYPE_DEVNULL = "devnull"
+
+_writer = None
+def getWriter():
+    global _writer
+    if not _writer:
+        writerType = os.getenv("KEYFRAME_EVENT_WRITER_TYPE",
+                               WRITER_TYPE_KINESIS)
+        if writerType == WRITER_TYPE_FILE:
+            _writer = FileWriter()
+        elif writerType == WRITER_TYPE_DEVNULL:
+            _writer = DevNullWriter()
+        elif writerType == WRITER_TYPE_KINESIS:
+            _writer = KinesisStreamWriter()
+        else:
+            raise Exception("Unknown event writer type (%s)", writerType)
+    return _writer
+
+class Writer(object):
+    def write(self, eventJson):
+        raise NotImplementedError()
+
+class DevNullWriter(Writer):
+    def write(self, data, partitionKey=None):
+        log.debug("devnull writer dropping event")
+
+
+class FileWriter(Writer):
+    def __init__(self, f=None):
+        self.f = f
+        if not f:
+            ts = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            self.f = os.getenv(
+                "KEYFRAME_EVENT_WRITER_FILE",
+                "/tmp/keyframe_events.%s.txt" % (ts,))
+        log.info("opening file for writer: %s", self.f)
+        self.fd = open(self.f, "a")
+
+    def write(self, data, partitionKey=None):
+        self.fd.write("%s\n" % (data,))
+        self.fd.flush()
+
+    def _close(self):
+        self.fd.close()
+        log.info("closing file event writer (%s)", self.f)
+
+    def __del__(self):
+        self._close()
+
+class KinesisStreamWriter(Writer):
+    def __init__(self, config=None, kinesisStreamName=None):
+        self.config = config
+        if not self.config:
+            self.config = keyframe.config.getConfig()
+        self.kstream = boto3.client(
+            'kinesis',
+            region_name=self.config.KINESIS_AWS_REGION,
+            aws_access_key_id=self.config.KINESIS_USER_ACCESS_KEY_ID,
+            aws_secret_access_key=self.config.KINESIS_USER_SECRET_ACCESS_KEY)
+        self.kinesisStreamName = kinesisStreamName
+        if not self.kinesisStreamName:
+            self.kinesisStreamName = self.config.KINESIS_STREAM_NAME
+
+    def write(self, data, partitionKey):
+        self.kstream.put_record(
+            StreamName=self.kinesisStreamName,
+            Data=data,
+            PartitionKey=partitionKey)
+
+
+def testFileWriter():
+    w = FileWriter()
+    w.write("event1")
+    w.write("event2")
+    w._close()
+    w2 = FileWriter(f=w.f)
+    w2.write("event3")
+
+def testKinesisWriter():
+    w = KinesisStreamWriter()
+    w.write(json.dumps({"event_id":"1234", "user_id":"u1"}), "u1")
+    w.write(json.dumps({"event_id":"1235", "user_id":"u2"}), "u2")
+    w.write(json.dumps({"event_id":"1236", "user_id":"u3"}), "u3")
+    w.write(json.dumps({"event_id":"1237", "user_id":"u1"}), "u1")
+
