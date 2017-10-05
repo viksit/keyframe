5bd2ee36cdeb0b8643961e06edac4dc8de485672
Nishant Deshpande <nishant@myralabs.com>
2017-10-05 02:17:04 -0700

    sessions and queries data for the db

--- events_analytics/session_processor.py
+++ events_analytics/session_processor.py
@@ -1,8 +1,10 @@
 import sys
 import json
+import copy
 import logging
 
-log = logging.getLogger(__name__)
+#log = logging.getLogger(__name__)
+log = logging.getLogger("keyframe.session_processor")
 
 class SessionProcessorError(Exception):
     pass
@@ -39,51 +41,83 @@ def processSession(session):
     """A session is a *chronological* list of events from a single session.
     Return the processed data required for analytics.
     """
-    kb_results = []
-    session_summary = {"kb_results": kb_results}
+    kb_info = []
+    session_summary = {
+        "ts":None,  # seconds from epoch as float
+        "kb_info": kb_info,  # [kb_search,..]
+        "num_kb_queries": 0,
+        "num_kb_negative_surveys": 0,
+        "topic": None,
+        "ticket_filed": False,
+        "ticket_url": None
+    }
 
     session_id = None
     lastSearch = None
     for event in session:
         log.debug("event: %s", event)
+        if event["version"] < 3:
+            log.info("Session contains event at version %s. Skipping this session.", event["version"])
+            return None
+
         if not session_id:
             session_id = event["session_id"]
         elif session_id != event["session_id"]:
             raise SessionProcessorError(
                 "current session_id: %s, new session_id: %s",
                 session_id, event["session_id"])
-            
-        if event["version"] < 3:
-            log.info("Session contains event at version %s. Skipping this session.", event["version"])
-            return None
+        if not "ts_ms" in session_summary:
+            session_summary["ts"] = float(event["ts_ms"])/1000
+        
         if isSearchSlot(event):
             log.debug("found search slot")
-            d = {"session_id": event["session_id"],
-                 "ts_ms": event["ts_ms"]}
-            kb_results.append(d)
+            # This is the schema for each kb_search.
+            kb_search = {
+                "session_id": event["session_id"],
+                "ts": float(event["ts_ms"])/1000,
+                "query":None,
+                "results":[],
+            }
+            kb_info.append(kb_search)
             # If format is not as expected, throwing exception is what I want.
             _tmp = event["payload"]["responseElements"][0]["responseMeta"]["searchAPIResult"]
-            d["query"] = _tmp["original_query"]
-            d["results"] = []
+            kb_search["query"] = _tmp["original_query"]
             for r in _tmp.get("hits", []):
-                d["results"].append({
+                kb_search["results"].append({
                     "title":r.get("title"),
                     "url":r.get("url"),
                     "snippets":r.get("snippets")})
-            lastSearch = d
+            lastSearch = kb_search
         if isSearchSurveySlot(event):
             log.debug("found search survey slot")
             if not lastSearch:
                 raise SessionProcessorError(
                     "No search before survey")
-            lastSearch["survey_results"] = event["payload"]["value"]
+            survey_value = event["payload"].get("value")
+            lastSearch["survey_results"] = survey_value
+            if survey_value.lower() in ("no", False):
+                session_summary["num_kb_negative_surveys"] += 1
         zendeskTicket = getTicket(event)
         if zendeskTicket:
-            session_summary["ticket_meta"] = zendeskTicket
+            session_summary.update(zendeskTicket)
+        if session_summary["kb_info"]:
+            session_summary["topic"] = session_summary["kb_info"][0].get("query")
+            session_summary["num_kb_queries"] = len(session_summary["kb_info"])
     return session_summary
 
-                
-        
+def processSession2(session):
+    session_summary = processSession(session)
+    session_data = copy.deepcopy(session_summary)
+    session_data.pop("kb_info")
+    query_data = []
+    for kb_info in session_summary.get("kb_info",[]):
+        query_data.append(kb_info)
+    return {
+        "session_summary":session_summary,
+        "session_data":session_data,
+        "query_data":query_data}
+
+
 def test():
     if not len(sys.argv) > 1:
         print >> sys.stderr, "Need a session file"
