34ed82ac57a399bc87540833f621c38f5a45a23a
Nishant Deshpande <nishant@myralabs.com>
2017-10-11 17:48:39 -0700

    different account ids for dev/prod

--- events_analytics/events_to_db.sh
+++ events_analytics/events_to_db.sh
@@ -4,6 +4,14 @@
 source /home/ubuntu/events_analytics/analytics/bin/activate
 source /mnt/production/myra_env_set.sh
 
-# accountId = 7BbmKJgxsMKRuAcBjNA1Zo = demo@myralabs.com
-python /home/ubuntu/events_analytics/spark_event_processor.py write-to-db -accountId 7BbmKJgxsMKRuAcBjNA1Zo
+REALM=${REALM:-"dev"}
+if [[ "${REALM}" == "prod" ]]; then
+    # accountId = 7BbmKJgxsMKRuAcBjNA1Zo = demo@myralabs.com
+    python /home/ubuntu/events_analytics/spark_event_processor.py write-to-db -accountId 7BbmKJgxsMKRuAcBjNA1Zo
+else
+    python /home/ubuntu/events_analytics/spark_event_processor.py write-to-db -accountId 3rxCO9rydbBIf3DOMb9lFh  # nishant+dev
+    python /home/ubuntu/events_analytics/spark_event_processor.py write-to-db -accountId 3oPxV9oFXxzHYxuvpy56a9  # viksit+dev
+fi
+
+
 
